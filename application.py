9c96c339d6344f60cf962160818bd224cf73bd86
chanhong-dev <zxck03051@gmail.com>
2021-10-10 22:56:13 +0900

    테스트!1

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ cors = CORS(application, resources={r"/*": {"origins": "*"}})
 
 @application.route('/')
 def main():
-    return render_template("index.html")
+    return "테스트"
 
 @application.route('/fileupload', methods=['POST'])
 def file_upload():
